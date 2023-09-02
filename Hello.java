2afe2b8caab022a7c5a52b15cd54be992d26232f
MarianneDaleGarcia <mariannedaleagarcia@gmail.com>
2023-09-02 22:18:01 +0800

    adding some changes to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{ 
   public static void main(String[] args){
     System.out.prinln("Hello Software Engineering Course!");
-    System.out.println("My name is GARCIA, Marianne Dale!");
+    System.out.println("My name is GARCIA, Marianne Dale!");	
+	System.out.println ("Complicated world of SoftDev that is why it pays much!");
   }
 }
